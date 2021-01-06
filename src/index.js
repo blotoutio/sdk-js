86e786fe8cd5f9a952c48456678481f148f21ad5
NejcZdovc <nejc.zdovc@3zsistemi.si>
2021-01-06 14:04:44 +0100

    Simplify index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { setInitialConfiguration, initialize } from './utils'
 import { setUrl } from './common/endPointUrlUtil'
 import * as log from './common/logUtil'
 import { constants, isDevEvtCollect, isDevEvtStore } from './config'
-import { getSession, setLocal } from './storage'
+import { setLocal } from './storage'
 import { getTempUseValue, setTempUseValue } from './storage/sharedPreferences'
 import { setDevEvent, setEndDevEvent, setStartDevEvent } from './event/session'
 import { setSessionPHIEvent, setSessionPIIEvent } from './session/personal'
@@ -17,46 +17,28 @@ import { collectEvent } from './event'
   const SDK = function () {}
 
   SDK.prototype.logEvent = function (eventName, meta = {}, objectName = '') {
-    try {
-      if (isDevEvtStore) {
-        if (getSession(constants.SESSION_ID)) {
-          setDevEvent(eventName, objectName, meta)
-        }
-        return
-      }
+    if (isDevEvtStore) {
+      setDevEvent(eventName, objectName, meta)
+      return
+    }
 
-      if (isDevEvtCollect) {
-        collectEvent(eventName, {}, constants.DEV_EVENT)
-      }
-    } catch (e) {
-      log.error(e)
+    if (isDevEvtCollect) {
+      collectEvent(eventName, {}, constants.DEV_EVENT)
     }
   }
 
   SDK.prototype.startTimedEvent = function (eventName, meta = {}, objectName = '') {
-    try {
-      if (getSession(constants.SESSION_ID)) {
-        setStartDevEvent(eventName, objectName, meta)
-      }
-    } catch (e) {
-      log.error(e)
-    }
+    setStartDevEvent(eventName, objectName, meta)
   }
 
   SDK.prototype.endTimedEvent = function (eventName) {
-    try {
-      if (isDevEvtStore) {
-        if (getSession(constants.SESSION_ID)) {
-          setEndDevEvent(eventName)
-        }
-        return
-      }
+    if (isDevEvtStore) {
+      setEndDevEvent(eventName)
+      return
+    }
 
-      if (isDevEvtCollect) {
-        collectEvent(eventName, {}, constants.DEV_EVENT)
-      }
-    } catch (e) {
-      log.error(e)
+    if (isDevEvtCollect) {
+      collectEvent(eventName, {}, constants.DEV_EVENT)
     }
   }
 
@@ -64,46 +46,39 @@ import { collectEvent } from './event'
     if (!preferences) {
       return
     }
-    try {
-      setUrl(preferences.endpointUrl)
-      setInitialConfiguration(preferences)
-      initialize(false)
-      setRetentionData()
 
-      setTempUseValue(constants.SDK_TOKEN, preferences.token)
-      if (!checkManifest()) {
-        pullManifest()
-          .then(() => {})
-          .catch(function (error) {
-            log.error(error)
-          })
-      } else {
-        if (checkUpdateForManifest()) {
-          updateManifest()
-        }
+    setUrl(preferences.endpointUrl)
+    setInitialConfiguration(preferences)
+    initialize(false)
+    setRetentionData()
+
+    setTempUseValue(constants.SDK_TOKEN, preferences.token)
+    if (!checkManifest()) {
+      pullManifest()
+        .then(() => {})
+        .catch(function (error) {
+          log.error(error)
+        })
+    } else {
+      if (checkUpdateForManifest()) {
+        updateManifest()
       }
-
-      if (!checkGeo()) {
-        setGeoDetails()
-      }
-
-      const ref = document.referrer
-      if (ref) {
-        setReferrer(ref)
-      }
-
-      startEvents(window)
-    } catch (e) {
-      log.error(e)
     }
+
+    if (!checkGeo()) {
+      setGeoDetails()
+    }
+
+    const ref = document.referrer
+    if (ref) {
+      setReferrer(ref)
+    }
+
+    startEvents(window)
   }
 
   SDK.prototype.setPayingUser = function () {
-    try {
-      setLocal(constants.IS_PAYING_USER, true)
-    } catch (e) {
-      log.error(e)
-    }
+    setLocal(constants.IS_PAYING_USER, true)
   }
 
   SDK.prototype.getUserId = function () {
@@ -111,36 +86,24 @@ import { collectEvent } from './event'
   }
 
   SDK.prototype.logPIIEvent = function (eventName, meta = {}, objectName = '') {
-    try {
-      if (isDevEvtStore) {
-        if (getSession(constants.SESSION_ID)) {
-          setSessionPIIEvent(eventName, objectName, meta)
-        }
-        return
-      }
+    if (isDevEvtStore) {
+      setSessionPIIEvent(eventName, objectName, meta)
+      return
+    }
 
-      if (isDevEvtCollect) {
-        collectEvent(eventName, {}, constants.DEV_EVENT)
-      }
-    } catch (e) {
-      log.error(e)
+    if (isDevEvtCollect) {
+      collectEvent(eventName, {}, constants.DEV_EVENT)
     }
   }
 
   SDK.prototype.logPHIEvent = function (eventName, meta = {}, objectName = '') {
-    try {
-      if (isDevEvtStore) {
-        if (getSession(constants.SESSION_ID)) {
-          setSessionPHIEvent(eventName, objectName, meta)
-        }
-        return
-      }
+    if (isDevEvtStore) {
+      setSessionPHIEvent(eventName, objectName, meta)
+      return
+    }
 
-      if (isDevEvtCollect) {
-        collectEvent(eventName, {}, constants.DEV_EVENT)
-      }
-    } catch (e) {
-      log.error(e)
+    if (isDevEvtCollect) {
+      collectEvent(eventName, {}, constants.DEV_EVENT)
     }
   }
 
